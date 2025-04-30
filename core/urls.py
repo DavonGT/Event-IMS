e1844ee95fb290b8c26d6b511daa800bd7be0568
afable31 <afablewarren2004@gmail.com>
2025-04-30 16:20:45 +0800

    make nav bar

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from django.shortcuts import redirect
+
 
 urlpatterns = [
     path('', views.home, name='home'),
-
+    path('admin-dashboard/', views.home, name='admin_dashboard'),  # Placeholder
+    path('organizer-dashboard/', views.home, name='organizer_dashboard'),  # Placeholder
+    path('student-dashboard/', views.home, name='student_dashboard'),  # Placeholder
+    path('logout/', lambda request: redirect('login'), name='logout'),  # Dummy logout
 ]
