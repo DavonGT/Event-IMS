b61db88b7ac8f56e7e3cf4cb965afd59ea83010d
DavonGT <davonaseo@gmail.com>
2025-05-01 12:38:41 +0800

    cleaned up the urls

--- Settings/urls.py
+++ Settings/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
-    path('core/', include('core.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('', include('core.urls')),
     
 ]
