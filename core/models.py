5c9c22df2b3833b037eab096ab300453af6d1903
DavonGT <davonaseo@gmail.com>
2025-05-01 14:05:50 +0800

    jinja example

--- core/models.py
+++ core/models.py
@@ -1,14 +1,23 @@
 from django.db import models
 from django.conf import settings  # Import settings for custom user model
 
+class Organization(models.Model):
+    name = models.CharField(max_length=100)
+    acronym = models.CharField(max_length=10)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
 class Event(models.Model):
+
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     description = models.TextField()
     location = models.CharField(max_length=255)
     start_datetime = models.DateTimeField()
     end_datetime = models.DateTimeField()
-    host = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # Updated to use custom user model
-    attendees = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="attended_events")  # Updated
+    host = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  
 
     def __str__(self):
         return self.name
