ad8a8dc9959604b6e103d41c03a989ffccbe3171
afable31 <afablewarren2004@gmail.com>
2025-05-01 21:16:50 +0800

    fix the modal design

--- core/forms.py
+++ core/forms.py
@@ -1,16 +1,19 @@
 from django import forms
-from django.contrib.auth import get_user_model
 from .models import Event, Organization
 
 class EventForm(forms.ModelForm):
-
-    organization = forms.ModelChoiceField(queryset=Organization.objects.all())
+    organization = forms.ModelChoiceField(
+        queryset=Organization.objects.all(),
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
 
     class Meta:
         model = Event
-        widgets = {
-            'start_datetime': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
-            'end_datetime': forms.DateTimeInput(attrs={'type': 'datetime-local'})
-        }
         fields = ['name', 'description', 'location', 'start_datetime', 'end_datetime', 'organization']
-
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'location': forms.TextInput(attrs={'class': 'form-control'}),
+            'start_datetime': forms.DateTimeInput(attrs={'type': 'datetime-local', 'class': 'form-control'}),
+            'end_datetime': forms.DateTimeInput(attrs={'type': 'datetime-local', 'class': 'form-control'}),
+        }
