52026a5a9c4acaa66e81248578264ff23a6990ba
DavonGT <davonaseo@gmail.com>
2025-05-03 15:01:51 +0800

    fixed add event logic

--- core/forms.py
+++ core/forms.py
@@ -6,11 +6,13 @@ class EventForm(forms.ModelForm):
     class Meta:
         model = Event
         fields = [
+            'organization',
             'name', 'description', 'location', 
             'start_datetime', 'end_datetime', 'who_can_attend'
             ]
         
         widgets = {
+            'organization': forms.Select(attrs={'class': 'form-control'}),
             'name': forms.TextInput(attrs={'class': 'form-control'}),
             'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
             'location': forms.TextInput(attrs={'class': 'form-control'}),
