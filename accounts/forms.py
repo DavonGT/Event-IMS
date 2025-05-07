f02c0bb27605e06953a893885464804d5114a6b5
DavonGT <davonaseo@gmail.com>
2025-05-08 00:03:14 +0800

    changes on registration forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -23,3 +23,18 @@ class UserRegistrationForm(forms.ModelForm):
             if field_name != 'role':
                 field.label = None
                 field.widget.attrs.update({'placeholder': placeholders.get(field_name, '')})
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password and confirm_password and password != confirm_password:
+            raise forms.ValidationError("Passwords do not match.")
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
